35640879b5282402b2f6874bf7b0c0b767ca03dd
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-27 17:50:10 +0530

    Write room script to handle Main Menu and friends list.

---
+++ Lodo/Assets/photonFriendsListManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class photonFriendsListManager : MonoBehaviourPunCallbacks, IMatchmakingCallbacks
+{
+    public string[] friendsAuthIdArray;
+    
+    //Constantly search for friends.
+    void FixedUpdate(){
+        if(friendsAuthIdArray.Length != 0)
+            PhotonNetwork.FindFriends(friendsAuthIdArray);
+    }
+    
+    public void OnFriendListUpdate(List< FriendInfo > friendList){
+        base.OnFriendListUpdate(friendList);
+        
+        FriendInfo info;
+        for(int i = 0; i< friendList.Count; i++){
+            info = friendList[i];
+        }
+    }
+}
